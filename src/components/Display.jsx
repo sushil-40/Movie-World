ffed8982265696eb58734b07deff876a0b7d44ac
Sushil <grayson.9999@gmail.com>
2024-10-15 20:21:59 +1000

    Implementing filter properties( implementing Display [Drama, Action, All Button]

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,31 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MovieCard } from "./MovieCard";
 
 export const Display = ({ movieList }) => {
-  console.log(movieList);
+  const [displayList, setDisplayList] = useState([]);
+
+  /* We use useEffect here because we need to re render the whole
+   display of below components*/
+  useEffect(() => {
+    setDisplayList(movieList);
+  }, [movieList]);
+
+  const hnadleOnFilter = (mood) => {
+    if (mood === "all") {
+      return setDisplayList(movieList);
+    }
+    // const filterMv = movieList.filter((mv) => mv.mood === mood);
+    // setDisplayList(filterMv);
+
+    setDisplayList(movieList.filter((mv) => mv.mood === mood));
+  };
   return (
     <div className="container  mt-5 rounded">
       <div className="bg-dark p-3">
         <div className="row">
           <div className="col">
             <div className="btn-group" role="group" aria-label="Basic example">
-              <button type="button" className="btn btn-primary">
+              <button
+                onClick={() => hnadleOnFilter("all")}
+                type="button"
+                className="btn btn-primary"
+              >
                 All
               </button>
-              <button type="button" className="btn btn-warning">
+              <button
+                onClick={() => hnadleOnFilter("drama")}
+                type="button"
+                className="btn btn-warning"
+              >
                 Drama
               </button>
-              <button type="button" className="btn btn-info">
+              <button
+                onClick={() => hnadleOnFilter("action")}
+                type="button"
+                className="btn btn-info"
+              >
                 Action
               </button>
             </div>
-            <div className="mt-3 text-light"> 2 Movies listed</div>
+            <div className="mt-3 text-light">
+              {" "}
+              {displayList.length} Movies listed
+            </div>
           </div>
         </div>
 
         <div className="row mt-5">
           <div className="col d-flex justify-content-around gap-2 flex-wrap">
-            {movieList.map((item, i) => (
+            {displayList.map((item, i) => (
               <div className="" key={i}>
                 <MovieCard searchedMovie={item} />
               </div>
