680a6b23d92bf854f6b6f81ad1160c376e28c53d
Sushil <grayson.9999@gmail.com>
2024-10-16 20:03:36 +1000

    removing unnecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Display } from "./components/Display";
 import { Hero } from "./components/Hero";
 import {
   accessFromLocalSession,
-  stroeInLocalSession,
+  storeInLocalSession,
 } from "./utils/localStorage";
 
 function App() {
@@ -14,24 +14,26 @@ function App() {
     const mvList = accessFromLocalSession();
     mvList?.length && setMovieList(mvList);
   }, []);
-  const addMovieToList = (movie) => {
-    //remove po.ssible duplicate movies
-    const tempMv = movieList.filter((item) => item.imdbID !== movie.imdbID);
-    setMovieList([...tempMv, movie]);
 
-    //saving in browser session local storae
-    stroeInLocalSession([...tempMv, movie]);
+  const addMovieToList = (movie) => {
+    //remove  possible duplicate movie
+    const tempMv = movieList.filter((item) => item.imdbID !== movie.imdbID);
+
+    setMovieList([...tempMv, movie]);
+    storeInLocalSession([...tempMv, movie]);
   };
 
   const handleOnDeleteMovie = (imdbId) => {
-    confirm("Are you sure you want to delete this movie from the list") &&
+    confirm("Are you sure, you want to delete this movie form the list") &&
       setMovieList(movieList.filter((mv) => mv.imdbID !== imdbId));
   };
+
   return (
     <div className="wrapper">
-      {/* {    hero section} */}
+      {/* hero section  */}
       <Hero addMovieToList={addMovieToList} />
-      {/* {   Display Section} */}
+
+      {/* Display seciton */}
       <Display
         movieList={movieList}
         handleOnDeleteMovie={handleOnDeleteMovie}
