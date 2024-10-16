641e656cdece289c601ded5ce3c9b73bae261704
Sushil <grayson.9999@gmail.com>
2024-10-16 20:06:40 +1000

    cleaning comments

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -4,43 +4,40 @@ import { MovieCard } from "./MovieCard";
 export const Display = ({ movieList, handleOnDeleteMovie }) => {
   const [displayList, setDisplayList] = useState([]);
 
-  /* We use useEffect here because we need to re render the whole
-   display of below components*/
   useEffect(() => {
     setDisplayList(movieList);
   }, [movieList]);
 
-  const hnadleOnFilter = (mood) => {
+  const handleOnFilter = (mood) => {
     if (mood === "all") {
       return setDisplayList(movieList);
     }
-    // const filterMv = movieList.filter((mv) => mv.mood === mood);
-    // setDisplayList(filterMv);
 
     setDisplayList(movieList.filter((mv) => mv.mood === mood));
   };
+
   return (
-    <div className="container  mt-5">
+    <div className="container mt-5 ">
       <div className="bg-dark p-3 rounded">
         <div className="row">
           <div className="col">
             <div className="btn-group" role="group" aria-label="Basic example">
               <button
-                onClick={() => hnadleOnFilter("all")}
+                onClick={() => handleOnFilter("all")}
                 type="button"
                 className="btn btn-primary"
               >
                 All
               </button>
               <button
-                onClick={() => hnadleOnFilter("drama")}
+                onClick={() => handleOnFilter("dram")}
                 type="button"
                 className="btn btn-warning"
               >
                 Drama
               </button>
               <button
-                onClick={() => hnadleOnFilter("action")}
+                onClick={() => handleOnFilter("action")}
                 type="button"
                 className="btn btn-info"
               >
@@ -48,22 +45,23 @@ export const Display = ({ movieList, handleOnDeleteMovie }) => {
               </button>
             </div>
             <div className="mt-3 text-light">
-              {" "}
-              {displayList.length} Movies listed
+              {displayList.length} movies listed
             </div>
           </div>
         </div>
 
         <div className="row mt-5">
-          <div className="col d-flex justify-content-around gap-2 flex-wrap">
-            {displayList.map((item, i) => (
-              <div className="" key={i}>
-                <MovieCard
-                  searchedMovie={item}
-                  deleteFunc={handleOnDeleteMovie}
-                />
-              </div>
-            ))}
+          <div className="col-md  ">
+            <div className="d-flex justify-content-around gap-2 flex-wrap">
+              {displayList.map((item, i) => (
+                <div className="" key={i}>
+                  <MovieCard
+                    searchedMovie={item}
+                    deleteFunc={handleOnDeleteMovie}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
