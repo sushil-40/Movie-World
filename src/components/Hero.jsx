5a312aeb53660da84febc5e92b94498ad3105e4d
Sushil <grayson.9999@gmail.com>
2024-10-16 20:20:58 +1000

    a

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React, { useEffect, useRef, useState } from "react";
 import { MovieCard } from "./MovieCard";
 import { fetchFromAPI } from "../utils/axios";
